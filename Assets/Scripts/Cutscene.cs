397c9bd68689551b43b81beba5e4fef4f86d50d1
Joseph <luvtogame01@gmail.com>
2020-11-19 19:20:53 -0500

    Level 3 Complete, tutorials added

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+        }
+    }
+}
