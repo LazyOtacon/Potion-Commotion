5af88b4ffc81a02008ee9be74c2d13b28a76a387
Joseph <luvtogame01@gmail.com>
2020-11-19 20:37:45 -0500

    Added Functioning Menu with scrolling background

---
+++ Assets/Scripts/BGscroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGscroll : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Renderer renderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTextureOffset = new Vector2(Time.time * scrollSpeed, 0);
+    }
+}
---
+++ Assets/Scripts/MainMenuControlls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControlls : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //SceneManager.LoadScene("Level 1"); just writing name gives way to human errors
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+    }
+}
